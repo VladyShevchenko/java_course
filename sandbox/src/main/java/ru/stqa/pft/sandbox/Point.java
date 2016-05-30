e7746df128d00017bd320327b632c6effe3c420b
Vladyslav Shevchenko <vladyslav.shevchenko@mev.com>
2016-05-30 14:01:10 +0300

    Uncommented function

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -8,15 +8,14 @@ public class Point {
         this.x=x;
         this.y=y;
     }
-    /*
-    Способ 1. Через Функцию
+    //Способ 1. Через функцию
     public static double distance(Point p1, Point p2) {
         double x = p1.x - p2.x;
         double y = p1.y - p2.y;
         return Math.sqrt(x*x + y*y);
-    }*/
+    }
 
-    //Спосою 2. Через метод
+    //Способ 2. Через метод
     public double distance(Point p) {
         double distance;
         distance = Math.sqrt(((this.x - p.x) * (this.x - p.x)) + ((this.y - p.y) * (this.y - p.y)));
@@ -25,7 +24,8 @@ public class Point {
     public static void main(String[] args){
         Point p1 = new Point(8,5);
         Point p2 = new Point(6,4);
-     // Функция -  System.out.println("Distance between point a and point b is "+ distance(p1,p2) );
+    // Функция:
+        System.out.println("Distance between point a and point b is "+ distance(p1,p2) );
      // Meтод:
         System.out.println("Distance between point a and point b is "+ p1.distance(p2));
     }
