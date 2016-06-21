c6ed2cc2e3c3244034f398ae5d74665f5d54bbf0
Vladyslav Shevchenko <vladyslav.shevchenko@mev.com>
2016-06-21 15:02:27 +0300

    Select group by it's index

--- addressbook web tests/src/test/java/ru/stqa/pft/adressbook/tests/GroupModificationTests.java
+++ addressbook web tests/src/test/java/ru/stqa/pft/adressbook/tests/GroupModificationTests.java
@@ -1,5 +1,6 @@
 package ru.stqa.pft.adressbook.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.adressbook.model.GroupData;
 
@@ -11,13 +12,16 @@ public class GroupModificationTests extends TestBase{
     @Test
     public void testGroupModification() {
         app.getNavigationHelper().gotoGroupPage();
+        int before = app.getGroupHelper().getGroupCount();
         if (!app.getGroupHelper().isThereAGroup()) {
             app.getGroupHelper().createGroup(new GroupData("test1", null, null));
         }
-        app.getGroupHelper().selectGroup();
+        app.getGroupHelper().selectGroup(before -1);
         app.getGroupHelper().initGroupModificaton();
         app.getGroupHelper().fillGroupForm(new GroupData("test3", "test4", "test5"));
         app.getGroupHelper().updateGroup();
         app.getGroupHelper().returnToGroupPage();
+        int after = app.getGroupHelper().getGroupCount();
+        Assert.assertEquals(after, before);
     }
 }
