9c3e6e4777a21c27a15f2e2fe8dca8722f59a576
Vladyslav Shevchenko <vladyslav.shevchenko@mev.com>
2016-07-26 17:08:57 +0300

    Lesson 8.6

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
@@ -20,6 +20,7 @@ public class ApplicationManager {
 
     private String browser;
     private RegistrationHelper registrationHelper;
+    private MailHelper mailHelper;
 
 
     public ApplicationManager(String browser)  {
@@ -76,5 +77,12 @@ public class ApplicationManager {
 
         }
         return wd;
+    }
+    
+    public MailHelper mail(){
+        if(mailHelper == null){
+            mailHelper=new MailHelper(this);
+        }
+        return mailHelper;
     }
 }
--- mantis-tests/src/test/resources/config_inc.php
+++ mantis-tests/src/test/resources/config_inc.php
@@ -6,4 +6,6 @@ $g_db_username            = 'root';
 $g_db_password            = '';
 $g_default_timezone       = 'Europe/Berlin';
 $g_crypto_master_salt     = 'I3URrq7wRF+t9WppMe8hoGWPJzQF8jTEf/sFupo/BWk=';
-$g_signup_use_captcha     = OFF;
+$g_signup_use_captcha     =  OFF;
+$g_phpMailer_method       =  PHPMAILER_METHOD_SMTP;
+$g_smtp_host              = 'localhost';
